ca527e8edc1a7864239a9930108896ee10bc8f46
Anthony <tonynarciso2003@gmail.com>
2024-10-15 09:09:16 -0500

    arreglo constante

--- arreglo.cpp
+++ arreglo.cpp
@@ -6,27 +6,15 @@ using namespace std;
 
 int main() 
 {
-    int n, elemento;
-    cout << "Cuantos elementos deseas ingresar? ";
-    cin >> n;
+    int elemento;
+    int numeros[7] = {1, 24, 56, 5, 80, 40, 46 };  
 
-    int* numeros= new int[n];  
-
-    cout << "Ingresa " << n << " numeros:\n";
-    for (int i = 0; i < n; ++i) {
-        cin >> numeros[i];
-    }
-    cout<<"El arreglo es: ";
-     for (int i = 0; i < n; ++i) {
-        cout << numeros[i] << " ";
-    }
-    cout << endl;
-
+    
     // ORDENANDO EL ARREGLO CON EL METODO QUICKSORT
-    quickSort(numeros, 0, n-1);
+    quickSort(numeros, 0, 6);
 
     cout << "Los numeros ordenados son: ";
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < 7; ++i) {
         cout << numeros[i] << " ";
     }
     cout << endl;
@@ -35,7 +23,7 @@ int main()
     cout<<"Ingrese el elemento a buscar: ";
     cin >> elemento;
 
-    int resultado = busquedaBinaria(numeros, 0, n - 1, elemento);
+    int resultado = busquedaBinaria(numeros, 0, 5, elemento);
 
     if (resultado != -1) {
         cout << "Elemento encontrado en el indice: " << resultado << endl;
